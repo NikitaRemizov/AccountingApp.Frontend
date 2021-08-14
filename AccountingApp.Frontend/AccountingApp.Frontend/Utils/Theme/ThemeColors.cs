f1bdc2ceaf300be55ecd894a3b96049e6d0f2565
Nikita Remizov <nremizov98@gmail.com>
2021-08-14 18:04:37 +0300

    Create custom theme and improve pages layout

---
+++ AccountingApp.Frontend/AccountingApp.Frontend/Utils/Theme/ThemeColors.cs
@@ -0,0 +1,20 @@
+﻿namespace AccountingApp.Frontend.Utils.Theme
+{
+    public static class ThemeColors
+    {
+        public const string Yellow = "#F5CB5C";
+        public const string EerieBlack = "#242423";
+        public const string Jet = "#333533";
+        public const string Timberwolf = "#D2D6CA";
+        public const string Alabaster = "#E8EDDF";
+        public const string BistreBrown = "#886507";
+        public const string DarkGoldenrod = "#AE8209";
+        public const string MossGreen = "#859D58";
+        public const string WintergreenDream = "#608070";
+        public const string CandyPink = "#D96D81";
+        public const string SteelBlue = "#467AAF";
+        public const string CopperRed = "#C77557";
+        public const string Citrine = "#E3D83B";
+        public const string BlueGreen = "#0090B8";
+    }
+}
